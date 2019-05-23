c9d2955918706015ba7d72a5a21ef7efd03a6def
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-23 08:54:43 +0000

    Deploy dijs/countdowner to github.com/dijs/countdowner.git:gh-pages

---
+++ precache-manifest.434f629fedbae6df3883ac3a7e09729e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b03c6d778621f29ca9d32dc2a60734c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa30e1059c3f28c6aba6",
+    "url": "/static/css/main.1da5fc82.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "fa30e1059c3f28c6aba6",
+    "url": "/static/js/main.46e98ade.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

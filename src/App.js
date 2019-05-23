52a9020b2d18299ff8d719fcf495d43ff7285f52
heather van der Dys <heather.vanderdys@gmail.com>
2019-05-23 19:34:15 +0300

    Styles - I got stuck bc it is more than 100% width.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Coming Soon!  
+        <div className="Nav-bar">
+          <img className="main-logo" src="/images/logo.png" />
+          <h1>CountdownerBeta</h1>
+        </div>
+        <div className="main">
+          <div className="info">
+            <h2>Features Requested Thus Far</h2>
+            <p>• Dianamic Sizing from the corner</p>
+            <p>• A Color Slider in Settings For the main Color of each Theme</p>
+            <h4>Current Beta Version | beta.13</h4>
+            <div className="downloads">
+              <button href="/downloads/CountdownerBeta-1.0.0-beta.13.dmg">
+                Download the Beta for MAC
+              </button>
+              <button href="/downloads/CountdownerBeta Setup 1.0.0-beta.13.exe">
+                Download the Beta for PC
+              </button>
+            </div>
+          </div>
+          <div className="preview">
+            <img className="preview" src="/images/countdownerPreview.png" />
+          </div>
+        </div>
+        <div className="Footer">
+          <p>
+            <img className="leath-logo" src="/images/LeathRedSmall.png" />
+            If you are interested in helping with our beta test by providing
+            feedback on this app, please email team@vanderdys.design
+          </p>
+        </div>
       </header>
     </div>
   );

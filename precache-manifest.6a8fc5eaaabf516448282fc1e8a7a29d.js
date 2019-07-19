80066ed5f3703e78dd51dc458044c5aaa33b3ccc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-19 11:45:00 +0000

    Deploy dijs/countdowner to github.com/dijs/countdowner.git:gh-pages

--- precache-manifest.6a8fc5eaaabf516448282fc1e8a7a29d.js
+++ precache-manifest.6a8fc5eaaabf516448282fc1e8a7a29d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cee5eebb2954a77019744d1382ffd93c",
+    "revision": "eb6aa131f163206863d522faea79c3fd",
     "url": "/index.html"
   },
   {
-    "revision": "bc947bb1c248584c3cc6",
+    "revision": "43583bbb624692219ebc",
     "url": "/static/css/main.d57d9f7e.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.bd888890.chunk.js"
   },
   {
-    "revision": "bc947bb1c248584c3cc6",
-    "url": "/static/js/main.a313f47d.chunk.js"
+    "revision": "43583bbb624692219ebc",
+    "url": "/static/js/main.dad62637.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.715ce3bb81cea522fa6fa31a21f20ae6.js"
+  "/precache-manifest.6a8fc5eaaabf516448282fc1e8a7a29d.js"
 );
 
 self.addEventListener('message', (event) => {
